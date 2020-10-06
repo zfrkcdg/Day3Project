c3f103cff9cdfb5ffede16f996f44c5ea87228cb
Zafer <kocadagzafer@gmail.com>
2020-10-06 21:59:12 +0200

    added more awesone stuff

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -3,4 +3,11 @@ public class AwesomeTest {
     // red means it is untracted
     //green means it is under staging area
     //grey ignored
+    //when the color is normal, it means itis commitetd no changes
+
+    //i just pulled the new change from Github that i did not have locally
+    //and now project is in sync with remote repostory
+
+    //Now  i made more awesome changes and I AM ABOUT TO push this back to Github
+
 }
